a34082007ed90314eee1e842e9cf263d748de016
SadieGoddard <Sadie109@hotmail.com>
2022-06-21 10:00:53 +0100

    populateAffirmationsTable debug

---
+++ db/scripts/populateAffirmationsTable.js
@@ -0,0 +1,11 @@
+import { query } from "../index.js";
+import { affirmations } from "../../lib/data.js";
+
+async function populateAffirmationsTable () {
+for (let i =0; i<affirmations.length; i++) {
+    const res = await query(`INSERT INTO affirmations (content) VALUES($1) RETURNING *`, [affirmations[i].content])
+    console.log (res.rows[0].content,"populated affirmations table");
+}
+}
+
+populateAffirmationsTable();
