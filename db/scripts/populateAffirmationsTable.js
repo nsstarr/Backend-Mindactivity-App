26c4afeb452d38ffc773737846fbdc92dd513a51
SadieGoddard <Sadie109@hotmail.com>
2022-06-21 13:31:20 +0100

    inserted affirmations data into the table

--- db/scripts/populateAffirmationsTable.js
+++ db/scripts/populateAffirmationsTable.js
@@ -3,7 +3,7 @@ import { affirmations } from "../../lib/data.js";
 
 async function populateAffirmationsTable () {
 for (let i =0; i < affirmations.length; i++) {
-    const res = await query(`INSERT INTO affirmations (content) VALUES ($1) RETURNING *;`, [affirmations[i].content]);
+    const res = await query(`INSERT INTO codingAffirmations (content) VALUES ($1) RETURNING *;`, [affirmations[i].content]);
     console.log (res.rows[0].content,"populated affirmations table");
 }
 // const res = await query(`INSERT INTO affirmations (content) VALUES ('Kazeem') RETURNING *`)
