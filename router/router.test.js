9d8a434e50a441af930e8a7982fea9fde2a59a3e
edgarasmarkunas <markunas11@gmail.com>
2022-06-23 09:20:26 +0100

    added a few jest tests testing structure of json and grabbing the correct id

--- router/router.test.js
+++ router/router.test.js
@@ -1,21 +1,49 @@
 import app from '../app.js'
 import request from 'supertest'
-import {test, expect} from '@jest/globals'
+import {test, expect, describe} from '@jest/globals'
 
+beforeAll(done => {
+    done()
+  })
 
-
-describe('Get /mindactivity', function () {
-    it('send request to /affirmations/0',async function() {
-            await request(app)
-            .get('/affirmations')
-            .expect(200)
-            .expect(async (res) => {
-                const expected = {success: true, payload: expect.any(Object)};
-                
-                const actual = res.body
-                
-                expect(actual).toStrictEqual(expected);
-                
-        })
+describe("GET affirmations", function() {
+    test("Get request (status code: 200) and check that header is json", async function(){
+        const response = await request(app).get("/mindactivity");
+        expect(response.statusCode).toEqual(200);
+        expect(response.headers["content-type"]).toMatch(/json/);
     })
+
+    test("GET request responds with correct body structure", async function(){
+        const response = await request(app).get("/mindactivity");
+        const actual = response.body;
+        const expected = {
+            success: true,
+            payload: expect.arrayContaining([
+                {
+                    id: expect.any(Number),
+                    content: expect.any(String)
+                }
+            ])
+        };
+
+        expect(actual).toEqual(expected);
+    })
+
+    test("GET request with specific ID and correct body structure", async function(){
+        const response = await request(app).get("/mindactivity/4");
+        const actual = response.body;
+        const expected = {
+            success: true,
+            payload: [{
+                id: 4,
+                content: expect.any(String)
+            }]
+        };
+        expect(actual).toEqual(expected);
+    })
+})
+
+ afterAll(done => {
+    app.close();
+    done();
 });
