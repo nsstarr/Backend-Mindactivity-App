27273b39c1424a4d00bc79432d7e954f97b4ec5c
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-08-21 18:57:43 +0100

    Making changes in app.js file

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
 import express, { application } from "express";
-import cors from 'cors';
-import  affirmationsRouter  from "./router/affirmationRouter.js";
+import affirmationsRouter from "./router/affirmationRouter.js";
 import blogsRouter from "./router/blogsRouter.js";
 const app = express();
+import cors from 'cors';
 
+// //CORS
+// app.use((req, res, next) => {
+//   //Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
 
+//   //Set to true if you need the website to include cookies in the requests sent
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-//CORS 
-app.use((req, res, next) => {
-  //Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://week-9-project.herokuapp.com/"
-  );
-
-  //Set to true if you need the website to include cookies in the requests sent
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Control-Type, Accept"
-  );
-  // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "POST, PUT, DELETE, OPTIONS, GET");
-  next();
-});
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Control-Type, Accept"
+//   );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "POST, PUT, DELETE, OPTIONS, GET"
+//   );
+//   next();
+// });
 
 app.use(express.urlencoded({ extended: false }));
-
 
 //middleware
 
 //v1 displays the version number of this app, potentially can be placed at the start of root path
-app.use('/v1/mindactivity', affirmationsRouter)
+app.use("/v1/mindactivity", affirmationsRouter);
 app.use("/v1/blogs", blogsRouter);
 app.use(express.json());
 
 //test get request
-app.get('/api', (req, res) => {
-  res.json({message: 'Hello from the server!'});
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the server!" });
 });
 
 const PORT = process.env.PORT || 3001;
